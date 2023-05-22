cb271c141d161559a654c803a4e3fb3561955601
Alain Belair <belair.alain@gmail.com>
2023-05-22 08:57:39 -0400

    fix typo in about

--- demos/intro/about.php
+++ demos/intro/about.php
@@ -14,11 +14,11 @@ Header::addTo(Ui::layout(), ['title' => 'About Fohn-Ui', 'size' => 3]);
 
 $html = 'Fohn-Ui is a PHP framework that make use of utility-first css framework:
 <a href="https://tailwindcss.com/" target="_blank" class="text-purple-700 underline">Tailwind CSS</a>.
-This is a powerful combination for building and styling Web applications.';
+This is a powerful combination for building and styling your Web applications.';
 DemoApp::addParagraph(Ui::layout(), $html, false);
 
 $html = 'At the hearth of Fohn-Ui is the <code class="text-sm bg-gray-200 p-1 font-bold">Service\Ui::class</code>.
-This service main function is to supply various elements need by Fohn-Ui during the request life cycle:';
+This service main function is to supply various elements needed by Fohn-Ui during the request life cycle:';
 DemoApp::addParagraph(Ui::layout(), $html, false);
 
 $serviceEl = [
@@ -56,30 +56,29 @@ DemoApp::addLineInfo(Ui::layout(), 'Note that content is added to a page \'layou
     ->removeTailwind('mt-6')
     ->appendTailwind('mx-4');
 
-$html = 'This site is build using Fohn-Ui. If you wish to see how it is done, visit the
+$html = 'This actual web application is build using Fohn-Ui. If you wish to see how it is done, visit the
 <a href="https://github.com/fohn-group/fohn-ui.com" target="_blank" class="text-purple-700 underline">fohn-ui.com</a>  repository.';
 DemoApp::addParagraph(Ui::layout(), $html, false);
 
 // //////////////// CSS
 Header::addTo(Ui::layout(), ['title' => 'Default theme and css stylesheet', 'size' => 4])->appendTailwind('mt-6');
 
-$html = 'Because Fohn-ui is built around Tailwind CSS, it is possible to create themes using PHP. This site use the default
-Fohn-Ui theme and Tailwind CSS utilities set in css stylesheet are generated from this default theme.
-Complete new theme can easily be create; or new color recipes, for a specific theme, can be generated simply by overriding the default theme class.
-';
+$html = 'Since Fohn-Ui is built around Tailwind CSS, it is possible to create themes using PHP. Fohn-Ui allow you to create new themes,
+  change view\'s style or update color recipes for a specific theme.';
 DemoApp::addParagraph(Ui::layout(), $html);
-DemoApp::addLineInfo(Ui::layout(), ' Many views in Fohn-Ui, like Button, are styled using the default theme:');
+DemoApp::addLineInfo(Ui::layout(), ' For example, Fohn-Ui button element is styled with the default theme:');
 DemoApp::addCodeConsole(Ui::layout())->setTextContent('Ui::theme()->styleAs(\'Button\', [$btn])');
 
 $html = 'The default theme generated a minimal set of Tailwind CSS utilities. However, it is possible
- to customize the Tailwind CSS utitlies needed for a project. <br/><br/> For more information on how to create a custom stylesheet,
+ to customize the Tailwind CSS utilites needed for a project. <br/><br/> For more information on how to create a custom stylesheet,
  visit the <a href="https://github.com/fohn-group/fohn-css" target="_blank" class="text-purple-700 underline">fohn-css</a> repository.';
 DemoApp::addParagraph(Ui::layout(), $html, false);
 
 // /////////////// JS
 Header::addTo(Ui::layout(), ['title' => 'Javascript', 'size' => 4])->appendTailwind('mt-6');
-$html = 'Fohn-Ui comes with its own javascript package. The package contains functionalities needed for component user interface
- and ensure a good user experience. The package contents vary from simple utilities functions, jQuery plugins or more complex Vue component.';
+$html = 'Fohn-Ui comes with its own javascript package. The package contains functionalities needed for component\'s user interface
+ in order to ensure a good user experience. The package contents vary from simple utilities functions, jQuery plugins or more complex Vue component, like
+ Modal, Form or Table to name a few.';
 DemoApp::addParagraph(Ui::layout(), $html);
 
 Header::addTo(Ui::layout(), ['title' => 'Vue.js', 'size' => 5])->appendTailwind('mt-6');
@@ -89,10 +88,10 @@ This allows for incredible flexibility, specially using Tailwind CSS, in styling
 DemoApp::addParagraph(Ui::layout(), $html, false);
 
 Header::addTo(Ui::layout(), ['title' => 'jQuery', 'size' => 5])->appendTailwind('mt-6');
-$html = 'jQuery is directly integrate to Fohn-Ui, which means that it is possible to assign jQuery event and function to a Fohn-Ui view directly using PHP code.';
+$html = 'There is a direct integration between jQuery and Fohn-Ui. It is possible to assign jQuery event and function to a Fohn-Ui view directly using PHP code.';
 DemoApp::addParagraph(Ui::layout(), $html);
 DemoApp::addCodeConsole(Ui::layout())->setTextContent('Jquery::addEventTo($btn, \'click\')->executes([Jquery::withThis()->text(\'Hello\')])');
 
-$html = 'Fohn-Ui js package can be changed and adapt to your project. For more information on how to do this, visit
+$html = 'Fohn-Ui js package can be adapted to your project. For more information on how to do this, visit
 the <a href="https://github.com/fohn-group/fohn-js" target="_blank" class="text-purple-700 underline">fohn-js</a> repository.';
 DemoApp::addParagraph(Ui::layout(), $html, false);
