4fcc20ddaac4fac17579cb9c9777a6ad0de2ac10
Alain Belair <belair.alain@gmail.com>
2023-08-04 16:46:48 -0400

    Update tw-default.php

--- demos/tw-default.php
+++ demos/tw-default.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 /**
  * Generate Default Tailwind utilities to be included in css files via tailwind.config.js.
  */
-require_once __DIR__ . '/../vendor/autoload.php';
 
-use Fohn\Ui\Tailwind\Theme\Fohn;
+use Fohn\Ui\Service\Theme\Fohn;
+
+require_once __DIR__ . '/../vendor/autoload.php';
 
 file_put_contents('fohn-theme-tw.txt', Fohn::getThemeCss());
 echo 'theme css done!' . \PHP_EOL;
